43ee271e7f7a5559f9ab07badc0cb4f34c25fafe
Trond Albinussen <trond.albinussen@blinq.no>
2023-08-03 10:36:47 +0200

    EnturService modified

---
+++ src/services/EnTurService.ts
@@ -0,0 +1,53 @@
+
+export class EnTurService {
+
+  private url: string;
+  private clientName: string;
+  private stopName: string;
+
+  constructor(url: string, clientName: string, stopName: string) {
+    this.url = url;
+    this.clientName = clientName;
+    this.stopName = stopName;
+  }
+
+  getRoutes = async () => {
+    const query = this.getQuery();
+    await fetch(this.url, {
+      method: 'POST',
+      headers: {
+        'ET-Client-Name': this.clientName,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then(res => res.json())
+      .then(stopPlaceData => console.log(stopPlaceData))
+  }
+
+  private getQuery = () => {
+    return `{
+            stopPlace(id:"NSR:StopPlace:58189") {
+              name
+              id
+              estimatedCalls(numberOfDepartures: 20) {
+                expectedDepartureTime
+                aimedDepartureTime
+                destinationDisplay {
+                  frontText
+                }
+                serviceJourney {
+                  line {
+                    publicCode
+                    transportMode
+                  }
+                }
+              }
+            }
+          }
+          `;
+  }
+
+}
+
+

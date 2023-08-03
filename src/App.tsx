8c9b3ce2741e61cf059b0e2382ff224661e8fb7f
Trond Albinussen <trond.albinussen@blinq.no>
2023-08-03 10:36:11 +0200

    React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useTranslation } from 'react-i18next';
-import { EnTurService } from './services/EnTurService';
 import { z } from 'zod';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import ResponsiveAppBar from './ResponsiveAppBar';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoutes } from './services/RouteService';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
 const envSchema = z.object({
   VITE_ENTUR_API: z.string(),
@@ -19,23 +23,24 @@ const result = envSchema.safeParse({
 });
 
 function App() {
-  const { t } = useTranslation();
-
   if (!result.success) {
     return <pre>{JSON.stringify(result.error.issues, null, 2)}</pre>;
   }
 
-  const enturService = new EnTurService(
-    import.meta.env.VITE_ENTUR_API,
-    import.meta.env.VITE_APP_NAME,
-    import.meta.env.VITE_STOP_PLACE
-  );
-  void enturService.getRoutes();
-
   return (
-    <div>
-      <h1>{t('english')}</h1>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ResponsiveAppBar />
+        <Routes>
+          {AppRoutes.map((route) => (
+            <Route path={route.path} key={route.key} element={route.element}>
+              {route.label}
+            </Route>
+          ))}
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 

dbef4104adb65b5ee930ed2887a3c7e6aab3ed39
Trond Albinussen <trond.albinussen@blinq.no>
2023-08-03 09:54:54 +0200

    types

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,6 +1,6 @@
 import "i18next";
 // import all namespaces (for the default language, only)
-import enNs1 from "./i18n/locales/en/translation.json";
+import enNs1 from "../i18n/locales/en/translation.json";
 
 declare module "i18next" {
     interface CustomTypeOptions {

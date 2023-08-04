a50ea0faec97112ca321235e9e3b6ce6c1c53cb4
Trond Albinussen <trond.albinussen@blinq.no>
2023-08-04 14:34:22 +0200

    Some translations around

--- src/pages/Heating.tsx
+++ src/pages/Heating.tsx
@@ -1,11 +1,13 @@
 import { Typography } from '@mui/material';
 import Card from '@mui/material/Card';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 const Heating = () => {
+  const { t } = useTranslation();
   return (
     <Card>
-      <Typography>Heating</Typography>
+      <Typography>{t('heating')}</Typography>
       <Link to="/lights">Lights</Link>
     </Card>
   );
--- src/pages/Lights.tsx
+++ src/pages/Lights.tsx
@@ -1,12 +1,15 @@
 import { Typography } from '@mui/material';
 import Card from '@mui/material/Card';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 const Lights = () => {
+  const { t } = useTranslation();
   return (
     <Card>
-      <Typography>Lights</Typography>
+      <Typography>{t('lights')}</Typography>
       <Link to="/heating">Heating</Link>
+      <Link to="/bobbo">NotFound example</Link>
     </Card>
   );
 };

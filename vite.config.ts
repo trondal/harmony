0a279990173bc78bec66d608dd021056e60a767b
Trond Albinussen <trond.albinussen@blinq.no>
2023-09-26 13:03:25 +0200

    Docker images

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], test: { globals: true, environment: 'jsdom' }
+  plugins: [
+    react()
+  ],
+  server: {
+    host: true,
+    port: 5173
+  },
+  test: { globals: true, environment: 'jsdom' }
 })
